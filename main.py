059a3c611a28fd8957caaa894ebe89f90332d4bc
Jeandre Le Roux <jeandre.le.roux@lsd.co.za>
2020-05-13 14:05:30 +0200

    Error handling

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sys
 
-from ansible.parsing.vault import PromptVaultSecret, VaultLib, VaultSecret
+from ansible.parsing.vault import PromptVaultSecret, VaultLib, VaultSecret, AnsibleVaultError
 from ruamel.yaml import YAML
 from pprint import pprint
 
-with open(sys.argv[1], "rb") as pw_file:
-    pw = pw_file.read()
+if len(sys.argv) != 4:
+    print("Supply <password_file> <input_yaml> <output_yaml>")
+    exit(1)
+
+try: 
+    with open(sys.argv[1], "rb") as pw_file:
+        pw = pw_file.read()
+except FileNotFoundError:
+    print("Password file not found")
+    exit(1)
 
 vault_pw = VaultSecret(pw)
 vault_pw.load()
@@ -29,8 +37,16 @@ yaml.indent(mapping=2, sequence=4, offset=2)
 yaml.register_class(VaultSecret)
 
 
-with open(sys.argv[2], 'r') as orig:
-    y = yaml.load(orig)
+try:
+    with open(sys.argv[2], 'r') as orig:
+        try:
+            y = yaml.load(orig)
+        except AnsibleVaultError as e:
+            print("Failed to decrypt")
+            print(e)
+            exit(1)
+except FileExistsError:
+    print(f"Failed to open {sys.argv[2]}")
 
 with open(sys.argv[3], 'w') as dest:
     yaml.dump(y, dest)
